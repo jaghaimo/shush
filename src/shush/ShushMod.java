43723d2921dbbdbab0c5d9ac54b68b0541dacee5
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-01-19 11:05:32 +0000

    Add some config

--- src/shush/ShushMod.java
+++ src/shush/ShushMod.java
@@ -1,6 +1,7 @@
 package shush;
 
 import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
@@ -8,6 +9,13 @@ public class ShushMod extends BaseModPlugin {
 
     @Override
     public void onApplicationLoad() throws Exception {
-        Logger.getRootLogger().setLevel(Level.WARN);
+        Level rootLevel = getLevel("rootLogLevel");
+        Level comFsLevel = getLevel("vanillaLogLevel");
+        Logger.getRootLogger().setLevel(rootLevel);
+        Logger.getLogger("com.fs").setLevel(comFsLevel);
+    }
+
+    private Level getLevel(String key) {
+        return Level.toLevel(Global.getSettings().getString(key));
     }
 }
