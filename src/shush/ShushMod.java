c2bb7ae1c916cfb869d03818539a54fc8f359039
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-01-19 10:34:08 +0000

    Make a mod

---
+++ src/shush/ShushMod.java
@@ -0,0 +1,14 @@
+package shush;
+
+import com.fs.starfarer.api.BaseModPlugin;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class ShushMod extends BaseModPlugin {
+
+    @Override
+    public void onApplicationLoad() throws Exception {
+        Logger.getRootLogger().setLevel(Level.WARN);
+    }
+}
